68698b5e7fb937a7a1d540bb99d63518cf0400c2
skatingfred <skatingfred@free.fr>
2023-09-19 00:51:20 +0200

    JQ avec cartons et dates

--- jq.date.bash
+++ jq.date.bash
@@ -3,21 +3,29 @@
 JQ='.
 |.[0] as $BIG 
 |.[1] as $ID
-| $BIG
+| [ $BIG
 |[.[] 
 | { (.META.IDENTIFIANT+"-"+.META.CARTON) : (.DATE | capture("^.(?<YEAR>[0-9]{4})" )|.YEAR |tonumber)} 
 | to_entries
-| .[] ]
-|group_by(.key)
-|map(first+{value:( (map(.value|select(.!=0))|unique))})
-| .[] 
-|{(.key):([(.value|min), (.value|max)]|unique)}
-#| $ID
-#| .
-
+|.[]]
+| group_by(.key)
+| map(first+{value:( (map(.value|select(.!=0))|unique))})
+|.[] 
+| {(.key):([(.value|min), (.value|max)]|unique)} 
+] as $dates
 ##########################################################
 #|{(.key):([(.value|min), (.value|max)]|unique|join("-"))}
 ##########################################################
+|[$ID[]
+| select((.CARTONS)!=null)
+| { (.IDENTIFIANT) : (.CARTONS) }  
+| select(.[]
+| to_entries
+| select((.[].key|test("^_";"x"))==false)
+)] as $cartons
+| $cartons
+
+
 
 
 |.
