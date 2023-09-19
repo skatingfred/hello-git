f22ed01220d6f97370a9eacc1bccdd06901e494c
skatingfred <skatingfred@free.fr>
2023-09-19 19:50:47 +0200

    JQ deux tableaux dates et cartons avec CID comme clé

--- jq.date.bash
+++ jq.date.bash
@@ -3,16 +3,20 @@
 JQ='.
 |.[0] as $BIG 
 |.[1] as $ID
-| [ $BIG
+|($BIG
 |[.[] 
-| { (.META.IDENTIFIANT+"-"+.META.CARTON) : (.DATE | capture("^.(?<YEAR>[0-9]{4})" )|.YEAR |tonumber)} 
-| to_entries
-|.[]]
-| group_by(.key)
-| map(first+{value:( (map(.value|select(.!=0))|unique))})
+| { (.META.IDENTIFIANT): { (.META.CARTON+"_"+.META.DOC_ID) : ((.DATE | capture("^.(?<YEAR>[0-9]{4})" )|.YEAR |tonumber| select(.>0)|[.]) ) } }
+]
+| unique
+|[ group_by(keys)
+|.[]
+| group_by(.[]|keys)
 |.[] 
-| {(.key):([(.value|min), (.value|max)]|unique)} 
-] as $dates
+| reduce .[] as $a ({}; .[$a|keys[0]] [ $a[]|keys[0] ]+=$a[][])
+| { (keys[0]): { (.[keys[0]]|keys[0]): ([(.[keys[0]][]|min) , (.[keys[0]][]|max)]|unique) }}
+]
+| reduce .[] as $a ({}; .[$a|keys[0]] [ $a[]|keys[0] ]=$a[][])
+) as $dates
 ##########################################################
 #|{(.key):([(.value|min), (.value|max)]|unique|join("-"))}
 ##########################################################
@@ -23,19 +27,11 @@ JQ='.
 | to_entries
 | select((.[].key|test("^_";"x"))==false)
 )] as $cartons
+
 | $cartons
-
-
-
-
 |.
 '
 
 #JQ='.'
-
 jq -s "$JQ"<<<$(cat BIG.RAW.json bdoa.json)
 
-
-
-exit
-  id: (.message | capture("id (?<id>[[:digit:]]+)").id)
